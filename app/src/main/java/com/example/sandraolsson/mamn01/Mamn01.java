a82aa9f686d1e7c49f52e94b41a82eb02249516d
sandraolsson <dic12sol@student.lu.se>
2015-04-08 21:59:03 +0200

    Version 1.0 funkar som den ska

--- app/src/main/java/com/example/sandraolsson/mamn01/Mamn01.java
+++ app/src/main/java/com/example/sandraolsson/mamn01/Mamn01.java
@@ -1,39 +1,37 @@
 package com.example.sandraolsson.mamn01;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class Mamn01 extends ActionBarActivity {
+public class Mamn01 extends ActionBarActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mamn01);
-    }
+        Button b = (Button) findViewById(R.id.button_sandra);
+       // b.setText("hejsan hoppsan");
+        b.setOnClickListener(this);
+        Log.d(this.getLocalClassName(), "Nu är onCreate färdig");
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_mamn01, menu);
-        return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onClick(View v) {
+        Log.d(this.getLocalClassName(), "Click!, du är i onClick");
+        startActivity(new Intent(this, CompassActivity.class));
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(getLocalClassName(), "Tillbaka! onResume");
     }
 }
